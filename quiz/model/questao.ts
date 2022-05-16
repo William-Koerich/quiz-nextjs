f23636719afe6ab89cea0af71e455e7997ff473d
William Koerich <williamkoerich17@gmail.com>
2022-05-16 11:20:33 -0300

    feat: created method to transform in literal object

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -39,4 +39,13 @@ export default class QuestaoModel {
 
         return false
     }
+
+    converterParaObjeto() {
+        return {
+            id: this.id,
+            enunciado: this.enunciado,
+            respostas: this.respostas.map(resposta => resposta.converterParaObjeto()),
+            acertou: this.acertou,
+        }
+    }
 }
