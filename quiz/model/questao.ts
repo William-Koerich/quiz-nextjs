ea82477c45eeec278b07a932d96d64a317a0b3f3
William Koerich <williamkoerich17@gmail.com>
2022-05-16 12:46:55 -0300

    feat: created api

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -1,3 +1,4 @@
+import { embaralhar } from "../functions/arrays"
 import RespostaModel from "./resposta"
 
 export default class QuestaoModel {
@@ -5,13 +6,14 @@ export default class QuestaoModel {
     #enunciado: string
     #respostas: RespostaModel[]
     #acertou: boolean
+    #respondida: boolean
 
-    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false, respondida = false) {
         this.#id = id
         this.#enunciado = enunciado
         this.#respostas = respostas
         this.#acertou = acertou
-        // this.#respondida = false
+        this.#respondida = respondida
     }
 
     
@@ -40,12 +42,30 @@ export default class QuestaoModel {
         return false
     }
 
+    responderCom(indice: number): QuestaoModel {
+        const acertou = this.respostas[indice]?.certa
+        const respostas = this.respostas.map((resposta, i) => {
+            const respostaSelecionada = i === indice
+            // Se eu não quiser revelar qual é a resposta certa, basta remover a parte do || na linha de baixo.
+            const deveRevelar = respostaSelecionada || resposta.certa
+            return deveRevelar ? resposta.revelar() : resposta
+        })
+
+        return new QuestaoModel(this.id, this.enunciado, respostas, acertou)
+    }
+
+    embaralharRespostas(): QuestaoModel {
+        const respostasEmbaralhadas = embaralhar(this.respostas)
+        return new QuestaoModel(this.id, this.enunciado, respostasEmbaralhadas, this.acertou)
+    }
+
     converterParaObjeto() {
         return {
             id: this.id,
             enunciado: this.enunciado,
-            respostas: this.respostas.map(resposta => resposta.converterParaObjeto()),
             acertou: this.acertou,
+            respondida: this.respondida,
+            respostas: this.respostas.map(resposta => resposta.converterParaObjeto())
         }
     }
 }
--- quiz/pages/api/questoes/[id].ts
+++ quiz/pages/api/questoes/[id].ts
@@ -1,6 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
 import questoes from '../bancoDeQuestoes'
 export default  (req, res) => {
+    const  id = +req.query.id
+
+    const questoesSelecionadas = questoes.filter(questao => questao.id === id)
+
+    if(questoesSelecionadas.length === 1) {
+        const questao = questoesSelecionadas[0].embaralharRespostas()
+        const objeto = questao.converterParaObjeto()
+        res.status(200).json(objeto)
+    } else {
+        res.status(204).send()
+    }
+
+    
 
     res.status(200).json(questoes[0].converterParaObjeto())
 }
