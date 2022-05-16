9b91cc972010037c4409975c9217923b668923b6
William Koerich <williamkoerich17@gmail.com>
2022-05-16 10:49:25 -0300

    feat: created answer getter

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -1,11 +1,12 @@
+import RespostaModel from "./resposta"
+
 export default class QuestaoModel {
     #id: number
     #enunciado: string
-    #respostas: any[]
+    #respostas: RespostaModel[]
     #acertou: boolean
-    // #respondida: boolean
 
-    constructor(id: number, enunciado: string, respostas: any[], acertou = false) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
         this.#id = id
         this.#enunciado = enunciado
         this.#respostas = respostas
@@ -31,6 +32,11 @@ export default class QuestaoModel {
     }
 
     get respondida() {
+
+        for(const resposta of this.#respostas) {
+            if(resposta.revelada) return true
+        }
+
         return false
     }
 }
